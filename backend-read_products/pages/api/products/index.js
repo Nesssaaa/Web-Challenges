4110d957262ed93c96d86da8e1009b65f6d7a558
Vanessa Ehrlein <vanessa.ehrlein@posteo.de>
2024-02-12 10:16:23 +0100

    tested create

--- backend-read_products/pages/api/products/index.js
+++ backend-read_products/pages/api/products/index.js
@@ -4,10 +4,19 @@ import Product from "@/db/models/Product";
 export default async function handler(request, response) {
   await dbConnect();
 
+  if (request.method === "POST") {
+    try {
+      const newProduct = await Product.create(request.body);
+      return response.status(200).json(newProduct);
+    } catch (error) {
+      return response
+        .status(500)
+        .json({ message: "error creating new product" });
+    }
+  }
+
   if (request.method === "GET") {
     const products = await Product.find();
-    console.log(products);
-
     return response.status(200).json(products);
   }
 }
